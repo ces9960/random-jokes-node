b111d5b3c18189797441e48a832ebb39c4af4572
ces9960 <ces9960@rit.edu>
2021-02-16 23:33:40 -0500

    Completed phase 1

--- src/index.js
+++ src/index.js
@@ -1,21 +1,11 @@
-const jokes = [
-  { q: 'What do you call a very small valentine?', a: 'A valen-tiny!' },
-  { q: 'What did the dog say when he rubbed his tail on the sandpaper?', a: 'Ruff, Ruff!' },
-  { q: "Why don't sharks like to eat clowns?", a: 'Because they taste funny!' },
-  { q: 'What did the boy cat say to the girl cat?', a: "You're Purr-fect!" },
-  { q: "What is a frog's favorite outdoor sport?", a: 'Fly Fishing!' },
-  { q: 'I hate jokes about German sausages.', a: 'Theyre the wurst.' },
-  { q: 'Did you hear about the cheese factory that exploded in France?', a: 'There was nothing left but de Brie.' },
-  { q: 'Our wedding was so beautiful ', a: 'Even the cake was in tiers.' },
-  { q: 'Is this pool safe for diving?', a: 'It deep ends.' },
-  { q: 'Dad, can you put my shoes on?', a: 'I dont think theyll fit me.' },
-  { q: 'Can February March?', a: 'No, but April May' },
-  { q: 'What lies at the bottom of the ocean and twitches?', a: 'A nervous wreck.' },
-  { q: 'Im reading a book on the history of glue.', a: 'I just cant seem to put it down.' },
-  { q: 'Dad, can you put the cat out?', a: 'I didnt know it was on fire.' },
-  { q: 'What did the ocean say to the sailboat?', a: 'Nothing, it just waved.' },
-  { q: 'What do you get when you cross a snowman with a vampire?', a: 'Frostbite' },
-];
+
+const htmlHandler = require('./htmlResponses.js');
+const jsonHandler = require('./jsonResponses.js');
+
+const urlStruct = {
+  '/random-joke' : jsonHandler.getRandomJokeResponse,
+  notFound : htmlHandler.get404Response,
+};
 
 // console.log("First web service starting up ...");
 
@@ -31,32 +21,10 @@ const port = process.env.PORT || process.env.NODE_PORT || 3000;
 // 4 - here's our index page
 
 // 5 - here's our 404 page
-const errorPage = `
-<html>
-    <head>
-        <title>404 - File Not Found!</title>
-    </head>
-    <body>
-        <h1>404 - File Not Found!</h1>
-        <p>Check your URL, or your typing!!</p>
-        <p>Perhaps you are looking for <a href="/random-joke">/random-joke</a> ?</p>
-        <p>:-O</p>
-    </body>
-</html>`;
 
 // 6 - this will return a random number no bigger than `max`, as a string
 // we will also doing our query parameter validation here
-const getRandomNumberJSON = (max = 1) => {
-  const number = Math.random() * max;
-  const jokeNumber = Math.floor(Math.random() * jokes.length);
-  const responseObj = {
-    timestamp: new Date(),
-    number,
-    question: jokes[jokeNumber].q,
-    answer: jokes[jokeNumber].a,
-  };
-  return JSON.stringify(responseObj);
-};
+
 
 // 7 - this is the function that will be called every time a client request comes in
 // this time we will look at the `pathname`, and send back the appropriate page
@@ -69,18 +37,13 @@ const onRequest = (request, response) => {
   // console.log("pathname=", pathname);
 
   // const params = query.parse(parsedUrl.query);
-  const max = 10;
   // console.log("params=", params);
   // console.log("max=", max);
 
-  if (pathname === '/random-joke') {
-    response.writeHead(200, { 'Content-Type': 'application/json' });
-    response.write(getRandomNumberJSON(max));
-    response.end();
-  } else {
-    response.writeHead(404, { 'Content-Type': 'text/html' });
-    response.write(errorPage);
-    response.end();
+  if(urlStruct[pathname]){
+    urlStruct[pathname](request,response);
+  }else{
+    urlStruct.notFound(request,response);
   }
 };
 
